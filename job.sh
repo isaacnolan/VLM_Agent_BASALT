5bef72979b1e689a642dc45e7b7bf7a60d68725a
EashanVytla <Eashan.Vytla@gmail.com>
2025-11-09 23:52:49 -0500

    taking full node

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=pas3150
 #SBATCH --job-name=minerl_job    # Job name
-#SBATCH --nodes=1 --ntasks-per-node=16
+#SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=1:00:00         # Time limit hrs:min:sec
 #SBATCH --output=logs/jobs/minerl_run_%j.log  # Standard output and error log (%j = job ID)
